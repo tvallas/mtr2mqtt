a33f0b3be1995f1fb4c18b78e0af46238a7b00ae
tvallas <tvallas@iki.fi>
2020-09-30 21:25:25 +0300

    refactor(metadata): fix pep8 style issues

--- mtr2mqtt/metadata.py
+++ mtr2mqtt/metadata.py
@@ -1,27 +1,42 @@
-import yaml
+"""
+Module for getting metadata for transmitters
+
+Functions
+    loadfile(file)
+    get_data(transmitter_id, all_transmitters)
+
+"""
+
 import logging
 import sys
+import yaml
 
 def loadfile(file):
+    """
+    Loads metadata file
+    """
     try:
-        with open(file,'r') as f:
-            transmitter_info = yaml.load(f, Loader=yaml.FullLoader)
+        with open(file,'r') as metafile:
+            transmitter_info = yaml.load(metafile, Loader=yaml.FullLoader)
             return transmitter_info
 
-
-    except FileNotFoundError as err:
-        logging.exception(f"File not found")
+    except FileNotFoundError:
+        logging.exception("File not found")
         sys.exit(-1)
 
 def get_data(transmitter_id, all_transmitters):
-    logging.debug(f"All transmitters metadata: {all_transmitters}")
-    logging.debug(f"Transmitter id: {transmitter_id}")
+    """
+    Gets metada for transmitter
+    """
+
+    logging.debug("All transmitters metadata: %s", all_transmitters)
+    logging.debug("Transmitter id: %s", transmitter_id)
     info = None
     for transmitter in all_transmitters:
-        logging.debug(f"Iterated transmitter: {transmitter}")
+        logging.debug("Iterated transmitter: %s", transmitter)
         if transmitter['id'] == transmitter_id:
             info = transmitter.copy()
             info.pop('id', None)
             break
-    logging.debug(f"Transmitter: {transmitter_id} metadata: {info}")
+    logging.debug("Transmitter: %s metadata: %s", transmitter_id, info)
     return info
