c2273d47b36e82757d97b87405b71d77da2b159e
tvallas <tvallas@iki.fi>
2021-03-21 21:41:46 +0200

    feat: add python-semantic-release package and change setup.py to use variable for versioning

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 with open('README.rst', encoding='UTF-8') as f:
     readme = f.read()
 
+__version__ = "0.0.0"
+
 setup(
     name='mtr2mqtt',
-    version='0.1.0',
+    version='__version__',
     description='MTR receiver readings to MQTT server',
     author='topo',
     author_email='tvallas@iki.fi',
