2faff7ec7708b19652aa9c02bbf1046dc0fc7838
semantic-release <semantic-release>
2026-04-16 17:08:37 +0000

    0.6.2
    Automatically generated by python-semantic-release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md', encoding='UTF-8') as f:
     readme = f.read()
 
-__version__ = "0.6.1"
+__version__ = "0.6.2"
 
 setup(
     name='mtr2mqtt',
